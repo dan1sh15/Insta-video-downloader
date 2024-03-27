53d2ef69ce9ba87b9d373d3b3c96ab4bbf07906c
dan1sh15 <danishqayyum9793@gmail.com>
2024-03-28 00:48:03 +0530

    new commit

--- src/components/DownloadSection.jsx
+++ src/components/DownloadSection.jsx
@@ -13,7 +13,7 @@ const DownloadSection = (props) => {
   return (
     <div className='grid mt-[10vh] grid-cols-2 max-ipad:grid-cols-1 gap-5 max-ipad:mr-[10rem] max-md:mr-0 items-center max-md:gap-y-5 justify-center h-auto '> 
       {
-          mediaArr.map((items, key) => (
+          mediaArr?.map((items, key) => (
             <div key={key} className='flex flex-col gap-y-5 w-full h-full p-5 bg-[#DCF2F1] rounded-lg'>
               <h1 className='text-2xl max-sm:text-xl text-center text-[#11235A] font-semibold capitalize'>Download Your {items.type.split('/')[0]} here</h1>
               <img loading='lazy' src={items.thumb} className='max-h-[400px] w-full object-contain' alt="Thumbnail Not Found" />
